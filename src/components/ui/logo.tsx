d14637db9d671a47bdd76f89924ef767f54b928d
jjribeiro1 <jvsr.99@hotmail.com>
2023-07-26 20:19:21 -0300

    refactor: logo width and height

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -4,7 +4,7 @@ import LogoImg from '../../../public/images/logo.png';
 
 export default function Logo() {
   return (
-    <div className="relative h-20 w-20 sm:h-32 sm:w-32 xl:h-36 xl:w-36">
+    <div className="relative h-20 w-20 sm:h-24 sm:w-24">
       <Image
         src={LogoImg}
         alt="logo do site"
