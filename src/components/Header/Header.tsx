3aec01ef058cc195fad5abfe9980932c5961f7b1
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-02 13:34:15 -0300

    fix: unused css class

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { NavBar } from './index';
 
 export default function Header() {
   return (
-    <header className="bg-mine-shaft-900 flex items-center px-4 py-2 pl-4">
+    <header className="bg-mine-shaft-900 flex items-center px-4 py-2">
       <Logo />
       <NavBar />
     </header>
