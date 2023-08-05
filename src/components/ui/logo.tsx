af1e86cecc765d785cae0720c836a31a36daccd8
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-05 13:01:19 -0300

    feat: link to home on logo click

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,22 +1,25 @@
-import Image from 'next/image';
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import LogoImg from '../../../public/images/logo.png';
 
 export default function Logo() {
   return (
     <div className="relative h-20 w-20 sm:h-24 sm:w-24">
-      <Image
-        src={LogoImg}
-        alt="logo do site"
-        quality={100}
-        priority
-        sizes="100vw"
-        style={{
-          width: '100%',
-          height: 'auto',
-          cursor: 'pointer',
-        }}
-      />
+      <Link href={'/'}>
+        <Image
+          src={LogoImg}
+          alt="logo do site"
+          quality={100}
+          priority
+          sizes="100vw"
+          style={{
+            width: '100%',
+            height: 'auto',
+            cursor: 'pointer',
+          }}
+        />
+      </Link>
     </div>
   );
 }
