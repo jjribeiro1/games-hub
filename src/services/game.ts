d853a2c5673f53f60ec1a03bbcf11a547c4ed6eb
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-05 20:12:24 -0300

    feat: add getGameById function to game service

--- src/services/game.ts
+++ src/services/game.ts
@@ -1,10 +1,11 @@
-import { collection, addDoc, getDocs, query, where } from 'firebase/firestore';
+import { collection, addDoc, getDocs, query, where, doc, getDoc } from 'firebase/firestore';
 import { StorageReference, getDownloadURL } from 'firebase/storage';
 import { db } from '@/firebase/config';
 import { uploadFile } from './storage';
 import { createGameTypeSchema } from '@/lib/schemas/create-game';
 import { AddDocFirebaseError } from '@/exceptions';
 import { GamesFilters } from '@/types/games-filters';
+import { Game } from '@/types/game';
 
 export async function createGame(game: createGameTypeSchema) {
   const thumbnailFile = await uploadFile(`/thumbnail/${game.thumbnail[0].name}`, game.thumbnail[0]);
@@ -34,4 +35,10 @@ export async function getAllGames() {
 export async function getGamesByFilters(filters: GamesFilters) {
   const q = query(collection(db, 'games'), where(filters.fieldPath, filters.operator, filters.value));
   return getDocs(q);
+}
+
+export async function getGameById(id: string) {
+  const docRef = doc(db, 'games', id);
+  const docSnap = await getDoc(docRef);
+  return docSnap.data() as Game;
 }
