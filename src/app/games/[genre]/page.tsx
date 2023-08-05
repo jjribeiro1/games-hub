345307f07e008f09964a2930429855968683728f
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-05 19:10:51 -0300

    move GameCard to a component

--- src/app/games/[genre]/page.tsx
+++ src/app/games/[genre]/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 import React from 'react';
-import Image from 'next/image';
-import { BsWindows } from 'react-icons/bs';
 import { usePathname } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
+import { GameCard } from '@/components/GameCard';
+import { getGamesByFilters } from '@/services/game';
 import { getOriginalGenreName } from '@/utils/format-genre-path';
 import { GamesFilters } from '@/types/games-filters';
-import { useQuery } from '@tanstack/react-query';
-import { getGamesByFilters } from '@/services/game';
 import { Game } from '@/types/game';
 
 export default function GamesGenrePage() {
@@ -20,8 +19,8 @@ export default function GamesGenrePage() {
   };
 
   const fetcher = async (filter: GamesFilters) => {
-    const games = await getGamesByFilters(filter);
-    return games.docs.map((doc) => doc.data()) as Game[];
+    const data = await getGamesByFilters(filter);
+    return data.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as Game[];
   };
 
   const { data } = useQuery({
@@ -35,36 +34,8 @@ export default function GamesGenrePage() {
 
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-items-center gap-y-4 gap-x-2 px-1 sm:px-4">
         {data?.map((game, i) => (
-          <li
-            key={i}
-            className="bg-mine-shaft-900 w-60 h-60 lg:w-72 lg:h-72 xl:w-80 xl:h-80 rounded-md cursor-pointer"
-          >
-            <Image
-              className="aspect-video rounded-t-md"
-              src={game.thumbnail}
-              alt={`imagem do jogo ${game.title}`}
-              width={320}
-              height={180}
-              priority
-            />
-
-            <div className="flex flex-col gap-2 p-2">
-              <p className="text-mine-shaft-200">{game.title}</p>
-              <p className="text-mine-shaft-400 whitespace-nowrap overflow-hidden overflow-ellipsis">
-                {game.short_description}
-              </p>
-
-              <div className="text-mine-shaft-200 flex items-center gap-2 self-end lg:mt-3">
-                <span className="text-xs lg:text-sm bg-mine-shaft-600 px-1 rounded">{game.genre}</span>
-                <span>
-                  {game.platform === 'Windows' ? (
-                    <BsWindows className="text-mine-shaft-400 w-5 h-5" />
-                  ) : (
-                    'Windows'
-                  )}
-                </span>
-              </div>
-            </div>
+          <li key={i}>
+            <GameCard game={game} />
           </li>
         ))}
       </ul>
---
+++ src/components/GameCard/GameCard.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from 'next/image';
+import { BsWindows } from 'react-icons/bs';
+import { Game } from '@/types/game';
+import Link from 'next/link';
+
+interface GameCardProps {
+  game: Game;
+}
+
+export default function GameCard({ game }: GameCardProps) {
+  return (
+    <div className="bg-mine-shaft-900 w-60 h-60 lg:w-72 lg:h-72 xl:w-80 xl:h-80 rounded-md cursor-pointer">
+      <Link href={`/game/${game.id}`} prefetch>
+      <Image
+        className="aspect-video rounded-t-md"
+        src={game.thumbnail}
+        alt={`imagem do jogo ${game.title}`}
+        width={320}
+        height={180}
+        priority
+      />
+      </Link>
+
+      <div className="flex flex-col gap-2 p-2">
+        <p className="text-mine-shaft-200">{game.title}</p>
+        <p className="text-mine-shaft-400 whitespace-nowrap overflow-hidden overflow-ellipsis">
+          {game.short_description}
+        </p>
+
+        <div className="text-mine-shaft-200 flex items-center gap-2 self-end lg:mt-3">
+          <span className="text-xs lg:text-sm bg-mine-shaft-600 px-1 rounded">{game.genre}</span>
+          <span>
+            {game.platform === 'Windows' ? <BsWindows className="text-mine-shaft-400 w-5 h-5" /> : 'Windows'}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
