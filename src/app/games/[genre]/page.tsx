85069f75749f02ccf1259f5389f8dae1f7ab653a
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-10 17:20:27 -0300

    feat: dynamic headingText

--- src/app/games/[genre]/page.tsx
+++ src/app/games/[genre]/page.tsx
@@ -10,9 +10,15 @@ export default function GamesGenrePage() {
   const originalGenre = getOriginalGenreName(params.genre as string) as string;
   const { games } = useFetchGamesByGenre(originalGenre);
 
+  const headingText = () => {
+    return `Top Free ${originalGenre} ${
+      originalGenre.includes(' Game') ? '' : 'games'
+    } for Pc and Browser in ${new Date().getFullYear()}`;
+  };
+
   return (
     <section className="flex flex-col gap-8 mt-6">
-      <h1 className="text-mine-shaft-200 text-center">Top Free MMORPG Games for PC and Browser In 2023!</h1>
+      <h1 className="text-mine-shaft-200 text-xl text-center">{headingText()}</h1>
 
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-items-center gap-y-4 gap-x-2 px-1 sm:px-4">
         {games?.map((game, i) => (
