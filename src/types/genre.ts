ba4da4835aa6b677f89bee67b88d065230cc11c2
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-12 00:04:49 -0300

    feat: add Genre and Platform interfaces

---
+++ src/types/genre.ts
@@ -0,0 +1,5 @@
+export interface Genre {
+  id: string;
+  name: string;
+  gamesId: string[];
+}
---
+++ src/types/platform.ts
@@ -0,0 +1,5 @@
+export interface Platform {
+  id: string;
+  name: string;
+  gamesId: string[];
+}
