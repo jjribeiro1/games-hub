426c730e1b83c8cded815e9108290689a89bb0e7
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-12 18:37:57 -0300

    feat: add FilterBar to GamesGenrePage

--- src/app/games/[genre]/page.tsx
+++ src/app/games/[genre]/page.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'next/navigation';
 import { GameCard } from '@/components/GameCard';
 import useFetchGamesByGenre from '@/hooks/useFetchGamesByGenre';
 import { getOriginalGenreName } from '@/utils/format-genre-path';
+import { FilterBar } from '@/components/FilterBar';
 
 export default function GamesGenrePage() {
   const params = useParams();
@@ -17,16 +18,22 @@ export default function GamesGenrePage() {
   };
 
   return (
-    <section className="flex flex-col gap-8 mt-6">
-      <h1 className="text-mine-shaft-200 text-xl text-center">{headingText()}</h1>
+    <main className="flex flex-col items-center gap-8 mt-6 px-4 w-full">
+      <h1 className="text-mine-shaft-200 text-xl">{headingText()}</h1>
 
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-items-center gap-y-4 gap-x-2 px-1 sm:px-4">
-        {games?.map((game, i) => (
-          <li key={i}>
-            <GameCard game={game} />
-          </li>
-        ))}
-      </ul>
-    </section>
+      <section className="">
+        <FilterBar />
+      </section>
+
+      <section className="w-full px-4">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-items-center gap-4">
+          {games?.map((game, i) => (
+            <li key={i}>
+              <GameCard game={game} />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
   );
 }
