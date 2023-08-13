9338b46f93a969cef7ecd41c3aa06455c4a69050
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-13 16:08:16 -0300

    feat: add getPlatformByName, create and update to platform service

--- src/services/platform.ts
+++ src/services/platform.ts
@@ -1,6 +1,24 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { addDoc, arrayUnion, collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
 import { db } from '@/firebase/config';
+import { Platform } from '@/types/platform';
 
 export async function getAllPlatforms() {
   return getDocs(collection(db, 'platforms'));
+}
+
+export async function getPlatformByName(name: string) {
+  const q = query(collection(db, 'platforms'), where('name', '==', name));
+  const docSnap = await getDocs(q);
+  return { id: docSnap.docs[0].id, ...docSnap.docs[0].data() } as Platform;
+}
+
+export async function createPlatform(name: string, gameId: string) {
+  await addDoc(collection(db, 'platforms'), { name, gamesId: [gameId] });
+}
+
+export async function updatePlatform(genreId: string, gameId: string) {
+  const genreRef = doc(db, 'platforms', genreId);
+  await updateDoc(genreRef, {
+    gamesId: arrayUnion(gameId),
+  });
 }
