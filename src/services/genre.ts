f0dbf3a653870ebfa41b8705ca6a1903aeb73de2
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-13 16:08:50 -0300

    feat: add getGenreByName, create and update to genre service

--- src/services/genre.ts
+++ src/services/genre.ts
@@ -1,6 +1,24 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { addDoc, arrayUnion, collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
 import { db } from '@/firebase/config';
+import { Genre } from '@/types/genre';
 
 export async function getAllGenres() {
   return getDocs(collection(db, 'genres'));
+}
+
+export async function getGenreByName(name: string) {
+  const q = query(collection(db, 'genres'), where('name', '==', name));
+  const docSnap = await getDocs(q);
+  return { id: docSnap.docs[0].id, ...docSnap.docs[0].data() } as Genre;
+}
+
+export async function createGenre(name: string, gameId: string) {
+  await addDoc(collection(db, 'genres'), { name, gamesId: [gameId] });
+}
+
+export async function updateGenre(genreId: string, gameId: string) {
+  const genreRef = doc(db, 'genres', genreId);
+  await updateDoc(genreRef, {
+    gamesId: arrayUnion(gameId),
+  });
 }
