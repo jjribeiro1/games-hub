d5a2e8d594bf9491fcf511abb441529865959dfa
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-13 16:09:35 -0300

    feat: add makeGameRelation function to game service

--- src/services/game.ts
+++ src/services/game.ts
@@ -2,6 +2,8 @@ import { collection, addDoc, getDocs, query, where, doc, getDoc } from 'firebase
 import { StorageReference, getDownloadURL } from 'firebase/storage';
 import { db } from '@/firebase/config';
 import { uploadFile } from './storage';
+import { createGenre, getAllGenres, getGenreByName, updateGenre } from './genre';
+import { createPlatform, getAllPlatforms, getPlatformByName, updatePlatform } from './platform';
 import { createGameTypeSchema } from '@/lib/schemas/create-game';
 import { AddDocFirebaseError } from '@/exceptions';
 import { GamesFilters } from '@/types/games-filters';
@@ -10,8 +12,7 @@ import { Game } from '@/types/game';
 export async function createGame(game: createGameTypeSchema) {
   const thumbnailFile = await uploadFile(`/thumbnail/${game.thumbnail[0].name}`, game.thumbnail[0]);
   const thumbnailRef = await getDownloadURL(thumbnailFile?.ref as StorageReference);
-
-  await addDoc(collection(db, 'games'), {
+  const createdGame = await addDoc(collection(db, 'games'), {
     title: game.title,
     genre: game.genre,
     release_date: new Date(game.release_date),
@@ -21,11 +22,44 @@ export async function createGame(game: createGameTypeSchema) {
     developer: game.developer,
     publisher: game.publisher,
     game_url: game.game_url,
+    min_system_requirements: {
+      os: game.min_system_requirements.os,
+      processor: game.min_system_requirements.processor,
+      graphics: game.min_system_requirements.graphics,
+      memory: game.min_system_requirements.memory,
+      storage: game.min_system_requirements.storage,
+    },
     isFree: game.isFree,
     thumbnail: thumbnailRef,
   }).catch(() => {
     throw new AddDocFirebaseError();
   });
+
+  await makeGameRelation(createdGame.id).catch(() => {
+    throw new Error('Erro ao criar as relações do jogo');
+  });
+}
+
+async function makeGameRelation(gameId: string) {
+  const gameData = await getGameById(gameId);
+  const genresSnapshot = await getAllGenres();
+  const platformsSnapshot = await getAllPlatforms();
+  const isNewPlatform = platformsSnapshot.docs.some((doc) => doc.data().name === gameData.platform) === false;
+  const isNewGenre = genresSnapshot.docs.some((doc) => doc.data().name === gameData.genre) === false;
+
+  if (isNewGenre) {
+    await createGenre(gameData.genre, gameData.id);
+  } else {
+    const genre = await getGenreByName(gameData.genre);
+    await updateGenre(genre.id, gameData.id);
+  }
+
+  if (isNewPlatform) {
+    await createPlatform(gameData.platform, gameData.id);
+  } else {
+    const platform = await getPlatformByName(gameData.platform);
+    await updatePlatform(platform.id, gameData.id);
+  }
 }
 
 export async function getAllGames() {
@@ -40,7 +74,7 @@ export async function getGamesByFilters(filters: GamesFilters) {
 export async function getGameById(id: string) {
   const docRef = doc(db, 'games', id);
   const docSnap = await getDoc(docRef);
-  return docSnap.data() as Game;
+  return { id: docSnap.id, ...docSnap.data() } as Game;
 }
 
 export async function getGamesByGenre(genreName: string) {
