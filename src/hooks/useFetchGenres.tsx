0e74ca25d795673b5fb53930f042ff9f480053ac
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-16 14:22:08 -0300

    refactor: useFetchGenres queryKey

--- src/hooks/useFetchGenres.tsx
+++ src/hooks/useFetchGenres.tsx
@@ -1,16 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { getAllGenres } from '@/services/genre';
-import { Genre } from '@/types/genre';
 
 export default function useFetchGenres() {
   const { data } = useQuery({
-    queryKey: ['fetch-genres'],
+    queryKey: ['get-all-genres'],
     queryFn: getAllGenres,
     staleTime: 1000 * 60 * 10,
     cacheTime: 1000 * 60 * 10,
   });
 
   return {
-    genres: data?.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as Genre[],
+    genres: data,
   };
 }
