b046a3ed5d555cb0cc2eb09da4790f7b2312a8d8
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-18 17:58:11 -0300

    feat: add getImageDownloadUrl function to storage service

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,10 +1,15 @@
 import { storage } from '@/firebase/config';
-import { ref, uploadBytes } from 'firebase/storage';
+import { StorageReference, getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { UploadImageError } from '@/exceptions';
 
 export async function uploadFile(storagePath: string, file: File) {
-  const fileRef = ref(storage, storagePath);
-  return uploadBytes(fileRef, file).catch(() => {
+  const storageRef = ref(storage, storagePath);
+
+  return uploadBytes(storageRef, file).catch(() => {
     throw new UploadImageError();
   });
+}
+
+export async function getImageDownloadUrl(ref: StorageReference) {
+  return await getDownloadURL(ref);
 }
