f9bd8b65dd8d3811df1e6dfd5cf474df5d14b7ac
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-18 20:08:59 -0300

    feat: add scale animation to GameCard component

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -13,30 +13,26 @@ export default function GameCard({ game, mappedGenres }: GameCardProps) {
   const { gameIcons } = useGameIcons(game);
 
   return (
-    <div className="bg-mine-shaft-900 w-60 h-60 lg:w-72 lg:h-72 xl:w-80 xl:h-80 rounded-md">
+    <div className="bg-mine-shaft-900 w-60 h-60 lg:w-72 lg:h-72 xl:w-80 xl:h-80 rounded-md hover:scale-105 transition-transform">
       <Link href={`/game/${game.id}`}>
-        <Image
-          className="aspect-video rounded-t-md"
-          src={game.thumbnail}
-          alt={`imagem do jogo ${game.title}`}
-          width={320}
-          height={180}
-          priority
-        />
+        <div className="flex flex-col gap-2">
+          <Image
+            className="aspect-video rounded-t-md"
+            src={game.thumbnail}
+            alt={`imagem do jogo ${game.title}`}
+            width={320}
+            height={180}
+            priority
+          />
+          <p className="text-mine-shaft-200 pl-2">{game.title}</p>
+        </div>
       </Link>
 
       <div className="flex flex-col gap-2 p-2">
-        <p className="text-mine-shaft-200">{game.title}</p>
         <p className="text-mine-shaft-400 whitespace-nowrap overflow-hidden overflow-ellipsis">
           {game.short_description}
         </p>
-
-        <div className="text-mine-shaft-200 flex items-center gap-2 self-end lg:mt-3">
-          <span className="text-xs lg:text-sm bg-mine-shaft-600 px-1 rounded">
-            {game.genre.map((genre) => mappedGenres.get(genre))}
-          </span>
-          <span className="flex items-center gap-2">{gameIcons()}</span>
-        </div>
+        <span className="flex items-center self-end gap-2 lg:mt-3 ">{gameIcons()}</span>
       </div>
     </div>
   );
