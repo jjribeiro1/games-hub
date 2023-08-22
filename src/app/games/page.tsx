920b16815de41eedd21f423c7cd0acc066615883
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-21 22:47:16 -0300

    feat: add Games page

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,5 +1,20 @@
+'use client';
 import React from 'react';
+import useFetchAllGames from '@/hooks/useFetchAllGames';
+import { GameCard } from '@/components/GameCard';
 
 export default function GamesPage() {
-  return <div>games page</div>;
+  const { games } = useFetchAllGames();
+
+  return (
+    <section className="w-full px-4">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-items-center gap-x-4 gap-y-6">
+        {games?.map((game) => (
+          <li key={game.id}>
+            <GameCard game={game} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
