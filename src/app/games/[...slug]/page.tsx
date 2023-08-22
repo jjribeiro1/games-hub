f64dfaab4b35cc5f6d96747c48a2877bf3f0c71e
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-21 22:46:56 -0300

    refactor: add game id as key of GameCard component

--- src/app/games/[...slug]/page.tsx
+++ src/app/games/[...slug]/page.tsx
@@ -23,8 +23,8 @@ export default function GamesSlugPage() {
   return (
     <section className="w-full px-4">
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-items-center gap-x-4 gap-y-6">
-        {games?.map((game, i) => (
-          <li key={i}>
+        {games?.map((game) => (
+          <li key={game.id}>
             <GameCard game={game} />
           </li>
         ))}
