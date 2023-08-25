e5dc0f3ce5aa9b07483e22b652e0002d3537d888
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-25 19:18:04 -0300

    refactor: move load more button to a section

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -10,32 +10,36 @@ export default function GamesPage() {
   const { data, isLoading, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage } = useFetchAllGames();
 
   return (
-    <section className="w-full flex flex-col gap-8">
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-items-center gap-y-6">
-        {isLoading ? (
-          <GameCardSkeleton quantity={8} />
-        ) : (
-          data?.pages.map((games) =>
-            games.games.map((game) => (
-              <li key={game.id}>
-                <GameCard game={game} />
-              </li>
-            )),
-          )
-        )}
-      </ul>
-      {data ? (
-        <Button
-          className="self-center bg-cyan-700 text-mine-shaft-100 hover:bg-cyan-800 text-lg font-semibold"
-          size={'lg'}
-          disabled={!hasNextPage || isFetchingNextPage}
-          onClick={() => fetchNextPage()}
-        >
-          {isFetchingNextPage ? 'Loading more...' : hasNextPage ? 'Load More' : 'Nothing more to load'}
-        </Button>
-      ) : null}
+    <>
+      <section>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-items-center gap-x-4 gap-y-6">
+          {isLoading ? (
+            <GameCardSkeleton quantity={8} />
+          ) : (
+            data?.pages.map((games) =>
+              games.games.map((game) => (
+                <li key={game.id}>
+                  <GameCard game={game} />
+                </li>
+              )),
+            )
+          )}
+        </ul>
+      </section>
+      <section>
+        {data ? (
+          <Button
+            className="self-center bg-cyan-700 text-mine-shaft-100 hover:bg-cyan-800 text-lg font-semibold"
+            size={'lg'}
+            disabled={!hasNextPage || isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+          >
+            {isFetchingNextPage ? 'Loading more...' : hasNextPage ? 'Load More' : 'Nothing more to load'}
+          </Button>
+        ) : null}
 
-      {isFetching && !isFetchingNextPage ? <Spinner /> : null}
-    </section>
+        {isFetching && !isFetchingNextPage ? <Spinner /> : null}
+      </section>
+    </>
   );
 }
