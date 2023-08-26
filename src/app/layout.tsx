759d643eec86af1f99334e8c5fba372a5b9e2802
jjribeiro1 <jvsr.99@hotmail.com>
2023-08-26 12:47:55 -0300

    feat: add Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import PageProviders from '@/providers';
 import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <PageProviders>
           <Header />
           {children}
+          <Footer />
         </PageProviders>
       </body>
     </html>
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-mine-shaft-900 text-lg text-mine-shaft-100 py-2 w-full">
+      <p className="text-center">Developed By João Vitor</p>
+    </footer>
+  );
+}
