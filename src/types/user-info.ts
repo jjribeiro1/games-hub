3c79f7ac0b7fec0cbcd8d13d9f50bf349ede17a8
jjribeiro1 <jvsr.99@hotmail.com>
2023-09-01 20:12:06 -0300

    feat: add library property to UserInfo interface

--- src/types/user-info.ts
+++ src/types/user-info.ts
@@ -1,5 +1,14 @@
+import { Game } from './game';
+
 export interface UserInfo {
   id: string;
   username: string;
   email: string;
+  library: UserLibrary[];
 }
+
+export type UserLibrary = {
+  game: Game;
+  type: GameInLibraryType;
+};
+export type GameInLibraryType = 'Uncategorized' | 'Currently Playing' | 'Completed' | 'Played' | 'Not Played';
