7cb934b7438e7d3d860dce2551c82a4b2f6dd6a7
jjribeiro1 <jvsr.99@hotmail.com>
2023-09-05 15:19:48 -0300

    feat: add accordion to display games in library page

--- src/app/my-library/page.tsx
+++ src/app/my-library/page.tsx
@@ -1,5 +1,36 @@
+'use client';
 import React from 'react';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+import useLoggedUserInfo from '@/hooks/useLoggedUserInfo';
+import { GameCard } from '@/components/GameCard';
+import { GameTypeInLibraryOption } from '@/types/user-info';
 
 export default function MyLibraryPage() {
-  return <div className="text-white">page</div>;
+  const { loggedUserInfo } = useLoggedUserInfo();
+  const library = loggedUserInfo?.library.map((game) => game);
+  const accordionGameTypeOptions: GameTypeInLibraryOption[] = [
+    'Uncategorized',
+    'Currently Playing',
+    'Completed',
+    'Played',
+    'Not Played',
+  ];
+
+  return (
+    <main className="h-screen p-4">
+      <Accordion type="multiple" className="w-full">
+        {accordionGameTypeOptions.map((option) => {
+          const games = library?.filter((game) => game.type === option);
+          return (
+            <AccordionItem key={option} value={option} disabled={games?.length === 0}>
+              <AccordionTrigger className="text-mine-shaft-100 text-lg">{`${option} (${games?.length})`}</AccordionTrigger>
+              <AccordionContent className="p-2">
+                {games?.map((game) => <GameCard key={game.id} game={game} loggedUserInfo={loggedUserInfo} />)}
+              </AccordionContent>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
+    </main>
+  );
 }
