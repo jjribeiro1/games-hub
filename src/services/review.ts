391dcbaf9abeb3ee379d44748de47503a6b33025
jjribeiro1 <jvsr.99@hotmail.com>
2023-09-11 22:44:12 -0300

    fix: string of review id

--- src/services/review.ts
+++ src/services/review.ts
@@ -2,12 +2,12 @@ import { db } from '@/firebase/config';
 import { collection, deleteDoc, doc, getDocs, query, setDoc, where } from 'firebase/firestore';
 
 export async function createReview(userId: string, gameId: string, rate: string, comment: string = '') {
-  const reviewId = `${userId}_${gameId}}`;
+  const reviewId = `${userId}_${gameId}`;
   await setDoc(doc(db, 'reviews', reviewId), { userId, gameId, rate, comment });
 }
 
 export async function deleteReview(userId: string, gameId: string) {
-  const reviewId = `${userId}_${gameId}}`;
+  const reviewId = `${userId}_${gameId}`;
   const docRef = doc(db, 'reviews', reviewId);
   await deleteDoc(docRef);
 }
