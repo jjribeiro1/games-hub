e88dc88975281fc60f409d2add47a3aaa02bde0f
jjribeiro1 <jvsr.99@hotmail.com>
2023-09-13 21:09:30 -0300

    feat: add updateReviewById

--- src/services/review.ts
+++ src/services/review.ts
@@ -1,8 +1,18 @@
 import { db } from '@/firebase/config';
-import { collection, deleteDoc, doc, getDoc, getDocs, query, setDoc, where } from 'firebase/firestore';
-import { Review } from '@/types/review';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+import { RateOptions, Review } from '@/types/review';
 
-export async function createReview(userId: string, gameId: string, rate: string, comment: string = '') {
+export async function createReview(userId: string, gameId: string, rate: RateOptions, comment: string = '') {
   const reviewId = `${userId}_${gameId}`;
   await setDoc(doc(db, 'reviews', reviewId), { userId, gameId, rate, comment });
 }
@@ -16,7 +26,6 @@ export async function deleteReview(userId: string, gameId: string) {
 export async function getAllReviewsFromUser(userId: string) {
   const q = query(collection(db, 'reviews'), where('userId', '==', userId));
   const snapshot = await getDocs(q);
-
   return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 }
 
@@ -29,4 +38,13 @@ export async function getReviewById(userId: string, gameId: string) {
   }
 
   return { id: docSnap.id, ...docSnap.data() } as Review;
+}
+
+export async function updateReviewById(userId: string, gameId: string, data: Partial<Review>) {
+  const { comment, rate } = data;
+  const docRef = doc(db, 'reviews', `${userId}_${gameId}`);
+  await updateDoc(docRef, {
+    comment,
+    rate,
+  });
 }
