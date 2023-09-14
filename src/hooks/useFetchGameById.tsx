91220487c774e111f0dd4dc7fcffba43331ebeab
jjribeiro1 <jvsr.99@hotmail.com>
2023-09-14 17:52:47 -0300

    feat: add useFetchGameById

---
+++ src/hooks/useFetchGameById.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import { getGameById } from '@/services/game';
+import { Game } from '@/types/game';
+
+export default function useFetchGameById(id: string) {
+  const { data, isLoading } = useQuery({
+    queryKey: ['getGameById', id],
+    queryFn: () => getGameById(id),
+    staleTime: 1000 * 60 * 10,
+    cacheTime: 1000 * 60 * 10,
+  });
+
+  return {
+    game: data as Game,
+    isLoading,
+  };
+}
