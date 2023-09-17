e46b81063f8235b2b9f266951af62e5980ed2ce3
jjribeiro1 <jvsr.99@hotmail.com>
2023-09-17 12:05:02 -0300

    refactor: GameImageSkeleton styles

--- src/app/game/[id]/GameImageSkeleton.tsx
+++ src/app/game/[id]/GameImageSkeleton.tsx
@@ -3,10 +3,10 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function GameImageSkeleton() {
   return (
-    <div className="flex flex-col gap-2 mt-4 px-4 w-[25%]">
-      <Skeleton className="w-full h-40 bg-mine-shaft-500" />
+    <div className="flex flex-col gap-2 px-4 w-full">
+      <Skeleton className="w-full h-48 bg-mine-shaft-500" />
       <div className="flex items-center gap-2">
-        <Skeleton className="w-[25%] h-10 bg-mine-shaft-500" />
+        <Skeleton className="w-[30%] h-10 bg-mine-shaft-500" />
         <Skeleton className="w-full h-10  bg-mine-shaft-500" />
       </div>
     </div>
