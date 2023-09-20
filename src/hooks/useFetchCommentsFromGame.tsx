ad3f8a6b0e901e4435059c35d6b834bc5ec62e6b
jjribeiro1 <jvsr.99@hotmail.com>
2023-09-20 17:12:13 -0300

    feat: adduseFetchCommentsFromGame hook

---
+++ src/hooks/useFetchCommentsFromGame.tsx
@@ -0,0 +1,16 @@
+import { getCommentsFromGame } from '@/services/comment';
+import { useQuery } from '@tanstack/react-query';
+
+export default function useFetchCommentsFromGame(gameId: string) {
+  const { data } = useQuery({
+    queryKey: ['get-comments-from-game'],
+    queryFn: () => getCommentsFromGame(gameId),
+    cacheTime: 1000 * 60 * 2,
+    staleTime: 1000 * 60 * 2,
+    enabled: !!gameId,
+  });
+
+  return {
+    commentsFromGame: data,
+  };
+}
