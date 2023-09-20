4945e8d897b354f7aa16790893333ea310412c82
jjribeiro1 <jvsr.99@hotmail.com>
2023-09-20 17:18:39 -0300

    feat: add useCreateComment mutation

---
+++ src/mutations/create-comment.ts
@@ -0,0 +1,22 @@
+import { useMutation } from '@tanstack/react-query';
+import { queryClient } from '@/providers';
+import { createComment } from '@/services/comment';
+import { CreateCommentInput } from '@/types/comment';
+import { toast } from 'react-toastify';
+
+interface MutationProps extends CreateCommentInput {}
+
+export default function useCreateComment() {
+  return useMutation({
+    mutationFn: ({ userId, username, gameId, text, createdAt }: MutationProps) =>
+      createComment({ userId, username, gameId, text, createdAt }),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['get-comments-from-game'] });
+    },
+
+    onError: () => {
+      toast.error('An unexpected error happened');
+    },
+  });
+}
