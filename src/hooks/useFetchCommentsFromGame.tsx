d1075e6f0cb69baf59f0e2e2e3e1d8935022376e
jjribeiro1 <jvsr.99@hotmail.com>
2023-09-20 19:39:49 -0300

    fix: add gameId parameter to query key

--- src/hooks/useFetchCommentsFromGame.tsx
+++ src/hooks/useFetchCommentsFromGame.tsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 
 export default function useFetchCommentsFromGame(gameId: string) {
   const { data } = useQuery({
-    queryKey: ['get-comments-from-game'],
+    queryKey: ['get-comments-from-game', gameId],
     queryFn: () => getCommentsFromGame(gameId),
     cacheTime: 1000 * 60 * 2,
     staleTime: 1000 * 60 * 2,
