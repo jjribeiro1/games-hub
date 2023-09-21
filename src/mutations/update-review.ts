94fda4358a845941caec35b5f1c04448eb353f6a
jjribeiro1 <jvsr.99@hotmail.com>
2023-09-21 16:00:07 -0300

    refactor: invalidate query "get-reviews-from-game"

--- src/mutations/update-review.ts
+++ src/mutations/update-review.ts
@@ -16,6 +16,7 @@ export function useUpdateReview() {
 
     onSuccess: (_, variables) => {
       queryClient.invalidateQueries({ queryKey: ['get-reviews-from-user', variables.userId] });
+      queryClient.invalidateQueries({ queryKey: ['get-reviews-from-game', variables.gameId] });
       toast.success('your updated review has been successfully submitted');
     },
 
