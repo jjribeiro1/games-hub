71a62631d2e08a0044d5ff731253d5d9a7c83a79
jjribeiro1 <jvsr.99@hotmail.com>
2023-10-04 16:16:17 -0300

    feat: add HeroSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { HeroSection } from '@/components/HeroSection';
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <HeroSection/>
+  );
 }
---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
+export default function HeroSection() {
+  return (
+    <section>
+      <div className="relative flex justify-center items-center w-[100%] h-80 md:h-[400px]">
+        <div className="absolute w-full h-full -z-50">
+          <Image
+            className="aspect-auto opacity-20"
+            src={'/images/heroImg.jpg'}
+            alt="sei lá"
+            fill
+            sizes="(min-width: 300px, 100%)"
+            priority
+          />
+        </div>
+
+        <div className="flex flex-col items-center justify-center gap-6">
+          <p className="text-2xl md:text-3xl text-mine-shaft-50 font-medium">
+            Discover the <em className="text-cyan-600">best</em> games here!
+          </p>
+          <p className="text-xs md:text-base text-mine-shaft-200 font-light">
+            {"Track what you've played and search for what to play next"}
+          </p>
+
+          <div className="flex items-center gap-3">
+            <Button
+              asChild
+              className="bg-cyan-600 hover:bg-cyan-700 text-mine-shaft-100 hover:text-mine-shaft-200 text-xs sm:text-base font-medium 
+               transition-colors"
+            >
+              <Link href={'/register'}>Get Started</Link>
+            </Button>
+            <Button
+              asChild
+              className="bg-transparent hover:bg-transparent border-2 border-mine-shaft-400 hover:border-mine-shaft-100 text-mine-shaft-400 
+              text-xs sm:text-base hover:text-mine-shaft-100  transition-colors"
+            >
+              <Link href={'/games'}>Browse Games</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
