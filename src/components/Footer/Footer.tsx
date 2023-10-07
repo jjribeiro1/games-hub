cd31b5f2c124e5f4c3ac73ed743429cffaff0c22
jjribeiro1 <jvsr.99@hotmail.com>
2023-10-06 21:33:40 -0300

    feat: link to github

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Github } from 'lucide-react';
 import { BackToTop } from '@/components/BackToTop';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="relative bg-mine-shaft-900 text-lg text-mine-shaft-100 py-2 w-full">
-      <p className="text-center">Developed By João Vitor</p>
+    <footer className="relative bg-mine-shaft-900 py-3 w-full">
+      <div className="text-sm flex items-center justify-center gap-1">
+        <span className="text-mine-shaft-100">Developed By</span>
+        <Link
+          href={'https://github.com/jjribeiro1'}
+          target="_blank"
+          className="flex items-center gap-0.5 text-mine-shaft-100 hover:text-mine-shaft-200"
+        >
+          jjribeiro1 <Github className="w-3.5 h-3.5" />
+        </Link>
+      </div>
       <BackToTop />
     </footer>
   );
