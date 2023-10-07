e571c10e265423399ae7e3d8a02bc2c90472024e
jjribeiro1 <jvsr.99@hotmail.com>
2023-10-06 22:52:32 -0300

    feat: add vercel analytics and change metadata

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 import AuthContextProvider from '@/context/auth-context';
+import { Analytics } from '@vercel/analytics/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
@@ -11,6 +12,7 @@ export default function PageProviders({ children }: { children: React.ReactNode 
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
         {children}
+        <Analytics/>
         <ToastContainer limit={1} position="top-right" autoClose={2000} />
       </AuthContextProvider>
     </QueryClientProvider>
